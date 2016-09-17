5af00a3e60e0c1b47846aa734160b206045ff512
jgarrison1204 <jgarrison1204@gmail.com>
2016-09-17 11:30:45 -0700

    Name and job title added

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,14 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+//Added  
+var name = "Jacob Garrison";
+var role = "Web Developer";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
+
