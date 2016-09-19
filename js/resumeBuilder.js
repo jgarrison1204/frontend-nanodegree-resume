74e5206ff89b81b7706b97572f542b0dbf63249b
jgarrison1204 <jgarrison1204@gmail.com>
2016-09-19 11:47:44 -0700

    Bio Object passed lint

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,17 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var bio = {
+	"name": "Jacob Garrison",
+	"role": "Web Developer",
+	"contacts": {
+		"mobile": "818-970-7261",
+		"email": "jgarrison1204@gmail.com",
+		"github":  "jgarrison1204",
+		"location": "Los Angeles"
+	},
+	"skills": ["Project Management", "CRM", "Data Analysis", "SQL", "Ruby", "VBA", "HTML", "CSS", "JS"],
+	"welcomeMessage": "Welcome!",
+	"picture": "images/fry.jpg"
+}
+
