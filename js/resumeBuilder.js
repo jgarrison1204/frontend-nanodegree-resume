10a3ab59ae81031e8ea720b7ebc00a6c34a6c7cb
jgarrison1204 <jgarrison1204@gmail.com>
2016-09-19 11:55:31 -0700

    Education section passes lint

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -12,6 +12,35 @@ var bio = {
 	},
 	"skills": ["Project Management", "CRM", "Data Analysis", "SQL", "Ruby", "VBA", "HTML", "CSS", "JS"],
 	"welcomeMessage": "Welcome!",
-	"picture": "images/fry.jpg"
+	"picture": "images/fry.jpg",
+}
+
+var education = {
+	"schools":[
+	{
+		"name": "University of Chicago",
+		"location": "Chicago",
+		"degree": "M.A.,",
+		"majors": ["Social Sciences"],
+		"dates": "2008-2009",
+		"url": "http://www.uchicago.edu/"
+	},
+	{
+		"name": "University of California San Diego",
+		"location": "La Jolla",
+		"degree": "B.A.",
+		"majors": ["Political Science", "History"],
+		"dates": "2003-2007",
+		"url": "https://ucsd.edu/"
+	}
+	],
+	"onlineCourses":[
+	{
+		"title": "JavaScript Basics",
+		"school": "Udacity",
+		"dates": "2016",
+		"url": "https://www.udacity.com/course/javascript-basics--ud804"
+	}
+	]
 }
 
